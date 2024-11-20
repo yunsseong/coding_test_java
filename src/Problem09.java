2521e2595d2bd30b7b19dcc565fee08f798ddb06
yunsseong <aster20@hanyang.ac.kr>
2024-11-20 13:54:46 +0900

    feat: 문제09 해결

---
+++ src/Problem09.java
@@ -0,0 +1,10 @@
+public class Problem09 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(8));
+    }
+
+    public static String solution(int decimal) {
+        return Integer.toBinaryString(decimal);
+    }
+}
